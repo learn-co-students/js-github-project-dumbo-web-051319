8fdc1cd312596541779b9896a5de886a505a1734
Joseph G Syverson <josephgavinsyverson@gmail.com>
2019-07-01 14:44:03 -0400

    | separate search bar for repos functional | to combine both searches into single bar for next commit |

--- js/index.js
+++ js/index.js
@@ -3,23 +3,25 @@
 const searchForm = document.querySelector('#github-form');
 const listOUsers = document.querySelector('#user-list');
 const listORepos = document.querySelector('#repos-list');
+const repoSearchForm = document.querySelector('#repo-form');
 
 // LISTENERS //////////////////////////////////////////////////////////////////
 
 searchForm.addEventListener("submit", nameForSearch);
-listOUsers.addEventListener("click", searchForRepos);
+listOUsers.addEventListener("click", searchForUserRepos);
+repoSearchForm.addEventListener("submit", getRepoName);
 
 // LOGIC //////////////////////////////////////////////////////////////////////
 
-function searchForRepos(event){
+function searchForUserRepos(event){
   nameInput = event.target.innerText.slice(10);
   const promprom = fetch(`https://api.github.com/users/${nameInput}/repos`);
   const respo = promprom.then((response) => response.json());
-  respo.then((json) => displayRepos(json, nameInput));
+  respo.then((json) => displayUserRepos(json, nameInput));
   // debugger;
 }
 
-function displayRepos(json, nameInput){
+function displayUserRepos(json, nameInput){
   const repos = json;
   listORepos.innerHTML = `<strong>${nameInput}'s Repos</strong>`;
   repos.forEach((repo) => {
@@ -41,11 +43,12 @@ function nameForSearch(event){
 function searchForUsers(nameInput){
   const promprom = fetch(`https://api.github.com/search/users?q=${nameInput}`);
   const respo = promprom.then((response) => response.json());
-  respo.then((json) => displaySearchResults(json));
+  respo.then((json) => displaySearchForUserResults(json));
   // debugger;
 }
 
-function displaySearchResults(json){
+
+function displaySearchForUserResults(json){
   const results = json.items;
   results.forEach((user) => {
     li = document.createElement('li');
@@ -56,5 +59,41 @@ function displaySearchResults(json){
         <br><br><br>`;
     // debugger;
     listOUsers.append(li);
+  });
+}
+
+function getRepoName(event){
+  event.preventDefault();
+  const repoNameInput = event.target.reposearch.value;
+  // debugger;
+  searchThroughAllRepos(repoNameInput);
+}
+
+function searchThroughAllRepos(repoNameInput){
+  fetch(`https://api.github.com/search/repositories?q=${repoNameInput}`)
+    .then((resp) => resp.json()).then((json) => displayRepos(json));
+}
+
+function displayRepos(json){
+  const repos = json.items;
+  // debugger;
+  repos.forEach((repo) => {
+    // debugger;
+    let name = repo.name;
+    let avatar = repo.owner.avatar_url;
+    let userName = repo.owner.login;
+    let linkURL = `https://github.com/${userName}/${name}`
+    let li = document.createElement('li');
+    li.innerHTML = `
+      <h3><a href=${linkURL}>${name}</a></h3>
+      <img src=${avatar} />
+      <br/>
+      <span>By ${userName}</span>
+      <br/>
+      <br/>
+    `;
+    listORepos.append(li);
+    // let avatar = repo.avatar_url;
+
   });
 }
